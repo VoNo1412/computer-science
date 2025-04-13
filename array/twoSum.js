d08f7785d0aae9891d6dd727367eec4f2016b08a
Vu Xuan Huy <xkaioken9x@gmail.com>
2025-04-13 23:40:21 +0700

    feat(PM-13): two sum solution 1

---
+++ array/twoSum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+    let result = null
+    nums.map((x, i) => {
+        let subNum = target - x;
+        if (map[subNum] >= 0) {
+            result = [map[subNum], i]
+        }
+        map[x] = i
+    })
+    return result
+};
+
+const result = twoSum([2,7,11,15], 9);
+// const result = twoSum([3,2,4], 6);
+// const result = twoSum([3,3], 6);
+console.log(result);
